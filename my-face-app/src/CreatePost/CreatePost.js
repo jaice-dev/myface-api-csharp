c0cf231efdb23038ca66750a57d1921130be8668
Jordan <jordan.d.carlton@gmail.com>
2021-10-22 11:31:04 +0100

    Add create posts POST request

--- my-face-app/src/CreatePost/CreatePost.js
+++ my-face-app/src/CreatePost/CreatePost.js
@@ -1,22 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import moment from "moment";
 import './CreatePost.scss'
+import { makePost} from "../apiRepo/postsRepo";
 
 const CreatePost = () => {
+
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        // const userId = event.target.userId;
+        // const message = event.target.Message;
+        // const imageUrl = event.target.ImageUrl;
+        // const time = moment(new Date()).format();
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs)
+        // alert(`UserId: ${inputs.UserId}. Message: ${inputs.Message} ImageUrl: ${inputs.ImageUrl}`)
+        makePost(inputs)
+    }
+
+
     return (
         <div className="post-form-container">
-            <form method="post" action="/posts/create">
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>Create a New Post</legend>
                     <label htmlFor="userId">User Id</label><br/>
-                    <input type="number" id="userId" name="userId" alt="user id input box"/><br/>
+                    <input type="number"
+                           id="userId"
+                           name="UserId"
+                           alt="user id input box"
+                           value={inputs.UserId}
+                           onChange={handleChange}
+                    /><br/>
 
-                    <label htmlFor="Message">Input Message</label><br/>
-                    <textarea id="Message" name="Message"/><br/>
+                    <label htmlFor="userMessage">Input Message</label><br/>
+                    <textarea id="userMessage"
+                              name="Message"
+                              value={inputs.Message}
+                              onChange={handleChange}
+                    /><br/>
 
-                    <label htmlFor="ImageUrl">Input Image Url</label><br/>
+                    <label htmlFor="imageUrl">Input Image Url</label><br/>
                     <div className={"image-url-input"}>
-                        <input id="ImageUrl-image" type="text" name="ImageUrl"/><br/>
-                        <input type="submit"/>
+                        <input id="imageUrl-image"
+                               type="text"
+                               name="ImageUrl"
+                               value={inputs.ImageUrl}
+                               onChange={handleChange}/><br/>
+                        <input type="submit" value={"Submit"}/>
                     </div>
                 </fieldset>
             </form>
--- my-face-app/src/apiRepo/postsRepo.js
+++ my-face-app/src/apiRepo/postsRepo.js
@@ -10,4 +10,22 @@ const getPosts = async () => {
     }
 }
 
-export { getPosts }
+const makePost = async (inputs) => {
+    try {
+        const response = await fetch("https://localhost:5001/posts/create", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(inputs)
+        })
+        //Message
+        //Image Url
+        //UserId (int)
+    } catch (error) {
+        console.log("Oh no, it went wrong", error)
+        throw error;
+    } finally {
+        console.log("Post Request finished")
+    }
+}
+
+export { getPosts, makePost }
